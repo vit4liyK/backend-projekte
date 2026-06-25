ab39fee7b116a57a7348bbcfab2c5aec0a444b9b
vitaliy <kellervitaliy@yahoo.de>
2026-06-25 11:21:41 +0200

    Lektion 20: CORS hinzugefügt

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const Database = require("better-sqlite3");
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Datenbank erstellen/öffnen
 const db = new Database("todos.db");
